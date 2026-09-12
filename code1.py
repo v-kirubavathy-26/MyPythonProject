31b3f8148179808f20ac706af50cd26ae3f1174a
kirubavathy <kirubaammu299@gmail.com>
2026-09-12 12:40:07 +0530

    Addedd

--- code1.py
+++ code1.py
@@ -3,9 +3,13 @@ num = int(input("Enter a number: "))
 if num <= 1:
     print("Not a prime number")
 else:
-    for i in range(2, num):
+    count = 0
+
+    for i in range(1, num + 1):
         if num % i == 0:
-            print("Not a prime number")
-            break
-    else:
+            count += 1
+
+    if count == 2:
         print("Prime number")
+    else:
+        print("Not a prime number")
