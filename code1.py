b708bea12a7f7b4ace8c58627a5af7e9ed3caaf3
kirubavathy <kirubaammu299@gmail.com>
2026-09-12 11:45:03 +0530

    Added prime and odd or even programs

--- code1.py
+++ code1.py
@@ -1,1 +1,11 @@
-print("This is Python Code 1")
+num = int(input("Enter a number: "))
+
+if num <= 1:
+    print("Not a prime number")
+else:
+    for i in range(2, num):
+        if num % i == 0:
+            print("Not a prime number")
+            break
+    else:
+        print("Prime number")
